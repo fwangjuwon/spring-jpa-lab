2bfe32056f77ea2500b9943243a7a2b8f0060caf
fwangjuwon <fwangjuwon@naver.com>
2022-03-08 12:15:48 +0900

    20220309 9. 쿠키 null point 해결

--- src/main/java/site/metacoding/dbproject/web/UserController.java
+++ src/main/java/site/metacoding/dbproject/web/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import site.metacoding.dbproject.domain.user.User;
 import site.metacoding.dbproject.domain.user.UserRepository;
@@ -20,7 +21,7 @@ import site.metacoding.dbproject.domain.user.UserRepository;
 @Controller
 public class UserController {
 
-    // 컴포지션(의존성 연결) : 컨트롤러는 레파지토리에 의존해야해!
+    // 컴퍼지션 (의존성 연결)
     private UserRepository userRepository;
     private HttpSession session;
 
@@ -30,91 +31,86 @@ public class UserController {
         this.session = session;
     }
 
-    // 회원가입 페이지 (정적) - 인증(로그인) X
+    // 회원가입 페이지 (정적) - 로그인X
     @GetMapping("/joinForm")
     public String joinForm() {
+        // 10초씩
         return "user/joinForm";
     }
 
-    // username=ssar&password=1234&email=ssar@nate.com (x-www 타입)
-    // 회원가입 INSERT - 인증(로그인) X
+    // username=ssar&password=&email=ssar@nate.com 패스워드 공백
+    // username=ssar&email=ssar@nate.com 패스워드 null
+    // username=ssar&password=1234&email=ssar@nate.com (x-www-form)
+    // 회원가입 - 로그인X
     @PostMapping("/join")
-    public String join(User user) { // 행위, 페이지 아님
-        // 1. username, password, email check -> choin form 에서 던진다. 문제!!서버 실행해보면 패스워드
-        // 안넣어도 회원가입이 돼버림
-        // -> 프론트에서 먼저 막아야한다. -일반인 (required 속성 걸기)
-        // -> 백엔드에서 막기 - 공격자(body로 넘어온 값을 확인: null check, 공백 check, @없는거 체크, 비밀번호 길이
-        // 체크,비번이 한글인지,,,,) -> null과 공백만 체크!!
-        // username=ssar&password=&email=ssar@nate.com 패스워드 공백
-        // username=ssar&email=ssar passwordnull
-        // user.getpassword==null
+    public String join(User user) {
 
-        // 1. username, password, email -> null check, 공백 check
-        // try-catch로 잡는다. if로 하면 최악이다 하나하나 다해야되
+        // 1. username, password, email 1.null체크, 2.공백체크
         if (user.getUsername() == null || user.getPassword() == null || user.getEmail() == null) {
-            return "redirect:/joinForm"; // 문제! 새로고침됨
+            return "redirect:/joinForm";
         }
         if (user.getUsername().equals("") || user.getPassword().equals("") || user.getEmail().equals("")) {
             return "redirect:/joinForm";
         }
 
-        // 2. 핵심 로직
+        // 2. 핵심로직
         User userEntity = userRepository.save(user);
         System.out.println("userEntity : " + userEntity);
-        // redirect는 GetMapping 주소!! redirect:매핑주소
+        // redirect:매핑주소
         return "redirect:/loginForm"; // 로그인페이지 이동해주는 컨트롤러 메서드를 재활용
     }
 
-    // 로그인 페이지 (정적) - 인증(로그인) X
+    // 로그인 페이지 (정적) - 로그인X
     @GetMapping("/loginForm")
     public String loginForm(HttpServletRequest request, Model model) {
-        // jsessionid=fijweofiwjeojwimekrw;remember=ssar
+        // JSESSIONID=asidaisdjasdi1233;remember=ssar
         // request.getHeader("Cookie");
-        Cookie[] cookies = request.getCookies(); // JSessionID, remember 두개가 있음
-        for (Cookie cookie : cookies) {
-            System.out.println("쿠키값: " + cookie.getName());
-            if (cookie.getName().equals("remembeer")) {
-                model.addAttribute("remember", cookie.getValue());
+
+        if (request.getCookies() != null) {
+            Cookie[] cookies = request.getCookies(); // JSESSIONID,remember 2개가 있음. 내부적으로 split해준것.
+            for (Cookie cookie : cookies) {
+                System.out.println("쿠키값 : " + cookie.getName());
+                if (cookie.getName().equals("remember")) {
+                    model.addAttribute("remember", cookie.getValue());
+                }
             }
         }
         return "user/loginForm";
     }
 
-    // 로그인 SELECT * FROM user WHERE username=? AND password=? -> 이런 메서드는 없으니까 직접
-    // 만들어!
-    // 원래 SELECT는 무조건 GET요청
-    // 근데 로그인만 예외! POST요청
-    // 이유 : 주소에 패스워드를 남길 수 없으니까!! 보안을 위해!!
-    // 로그인 - 인증(로그인) X
+    // SELECT * FROM user WHERE username=? AND password=?
+    // 원래 SELECT 는 무조건 get요청
+    // 그런데 로그인만 예외 (POST)
+    // 이유 : 주소에 패스워드를 남길 수 없으니까!!
+    // 로그인 - - 로그인X
     @PostMapping("/login")
-    public String login(HttpServletResponse response, User user) {
-        // HttpSession session = request.getSession(); // 쿠키에 JSESSIONID를 85로 가져오면
-        // session의 자기 공간을 가리킴
+    public String login(User user, HttpServletResponse response) {
 
         // 1. DB연결해서 username, password 있는지 확인
         User userEntity = userRepository.mLogin(user.getUsername(), user.getPassword());
 
-        // 2. 있으면 session 영역에 인증됨이라고 메시지 하나 넣어두자
+        // 2. 있으면 session 영역에 인증됨 이라고 메시지 하나 넣어두자.
         if (userEntity == null) {
             System.out.println("아이디 혹은 패스워드가 틀렸습니다.");
         } else {
             System.out.println("로그인 되었습니다.");
-            // 세션에 옮겨담자, request는 사라졌지만 세션영역에 보관
-            session.setAttribute("principal", userEntity); // principal 인증된 주체 -> 로그인
+            session.setAttribute("principal", userEntity); // session에 user의 정보를 기록!!
 
-            if (user.getRemember().equals("on")) {
-                response.setHeader("Set-Cookie", "remember=" + userEntity.getUsername());
+            if (user.getRemember() != null && user.getRemember().equals("on")) {
+                response.addHeader("Set-Cookie", "remember=" + user.getUsername());
             }
         }
-        return "redirect:/";
+
+        return "redirect:/"; // PostController 만들고 수정하자.
     }
 
     // http://localhost:8080/user/1
-    // 유저 상세 페이지 (동적 -> DB연동 필요) - 인증(로그인) O
+    // 유저상세 페이지 (동적) - 로그인O
     @GetMapping("/user/{id}")
-    public String detail(@PathVariable int id, Model model) {
+    public String detail(@PathVariable Integer id, Model model) {
 
-        // 유효성 검사하기 (수십개... 엄청 많음)
+        // 유효성 검사 하기 (수십개....엄청 많겠죠?)
+
         User principal = (User) session.getAttribute("principal");
 
         // 1. 인증 체크
@@ -122,40 +118,41 @@ public class UserController {
             return "error/page1";
         }
 
-        // 2. 권한 체크
+        // 2. 권한체크
         if (principal.getId() != id) {
             return "error/page1";
         }
-        // 3. 핵심 로직
-        Optional<User> userOp = userRepository.findById(id); // 유저정보
 
-        if (userOp.isPresent()) { // 박스안에 뭐가 있으면
+        // 3. 핵심로직
+        Optional<User> userOp = userRepository.findById(id);
+
+        if (userOp.isPresent()) {
             User userEntity = userOp.get();
             model.addAttribute("user", userEntity);
             return "user/detail";
-        } else { // 없으면 == isEmpty
+        } else {
             return "error/page1";
         }
 
-        // db에 로그 남기기(로그인한 아이디)
+        // DB에 로그 남기기 (로그인 한 아이디)
     }
 
-    // 유저 수정 페이지 - 인증(로그인) O
+    // 유저수정 페이지 (동적) - 로그인O
     @GetMapping("/user/updateForm")
     public String updateForm() {
         return "user/updateForm";
     }
 
-    // 유저 수정 - 인증(로그인) O
+    // 유저수정 - 로그인O
     @PutMapping("/user/{id}")
-    public String update(@PathVariable int id) {
+    public String update(@PathVariable Integer id) {
         return "redirect:/user/" + id;
     }
 
-    // 로그아웃 - 인증(로그인) O
+    // 로그아웃 - 로그인O
     @GetMapping("/logout")
     public String logout() {
-        session.invalidate(); // session 다 날림
-        return "redirect:/loginForm"; // PostController 만들고 수정하자
+        session.invalidate();
+        return "redirect:/loginForm"; // PostController 만들고 수정하자.
     }
 }
