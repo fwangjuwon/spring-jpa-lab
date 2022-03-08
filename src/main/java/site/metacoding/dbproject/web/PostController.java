feb79a446867ee550a6cd794dbf3c70808f553cb
fwangjuwon <fwangjuwon@naver.com>
2022-03-08 16:23:52 +0900

    20220308 10. 인증 필터링을 위한 주소 설계로 변경

--- src/main/java/site/metacoding/dbproject/web/PostController.java
+++ src/main/java/site/metacoding/dbproject/web/PostController.java
@@ -13,45 +13,45 @@ import site.metacoding.dbproject.domain.post.Post;
 @Controller
 public class PostController {
 
-    // 글쓰기 페이지 /post/writeForm
-    @GetMapping("/post/writeForm")
+    // 글쓰기 페이지 /post/writeForm- 인증 필요o, 권한 필요x
+    @GetMapping("/s/post/writeForm")
     public String writeForm() {
         return "post/writeForm"; // viewresolver도움 받음
     }
 
-    // main 페이지
+    // main 페이지 - 인증 필요x
     // 글 목록 페이지 /post/list, /
     @GetMapping({ "/", "/post/list" })
     public String list() {
         return "post/list";
     }
 
-    // 글 상세보기 페이지 /post/{id} (삭제버튼 -> 글목록으로 돌아오기 , 수정버튼->수정페이지 만들어 두기)
-    @GetMapping("/post/{id}")
+    // 글 상세보기 페이지 /post/{id} (삭제버튼 -> 글목록으로 돌아오기 , 수정버튼->수정페이지 만들어 두기) 인증 필요x
+    @GetMapping("/post/{id}") // get요청에 /post 만 제외 시키기 !! get은 일단 무사통과 시켜 !
     public String detail(@PathVariable Integer id) {
         return "post/detail";
     }
 
-    // 글 수정 페이지 /post/{id}/updateForm
-    @GetMapping("/post/{id}/updateForm")
+    // 글 수정 페이지 /post/{id}/updateForm 인증필요o
+    @GetMapping("/s/post/{id}/updateForm")
     public String updateForm(@PathVariable int id) {
         return "post/updateForm";
     }
 
-    // delete 글 삭제 /post/{id} -> 글 목록 페이지로
-    @DeleteMapping("/post/{id}")
+    // delete 글 삭제 /post/{id} -> 글 목록 페이지로 인증필요o
+    @DeleteMapping("/s/post/{id}")
     public String delete(@PathVariable Integer id) {
         return "redirect:/";
     }
 
-    // update 글 수정 /post/{id} -> 글상세보기 페이지
-    @PutMapping("/post/{id}")
+    // update 글 수정 /post/{id} -> 글상세보기 페이지 인증필요o
+    @PutMapping("/s/post/{id}")
     public String update(@PathVariable Integer id) {
         return "redirect:/post/" + id;
     }
 
-    // post글 쓰기 /post -> 글목록 페이지로
-    @PostMapping("/post")
+    // post글 쓰기 /post -> 글목록 페이지로 인증필요o
+    @PostMapping("/s/post")
     public String write() {
         return "redirect:/"; // redirect: 다른 페이지로 이동시켜준다.
     }
